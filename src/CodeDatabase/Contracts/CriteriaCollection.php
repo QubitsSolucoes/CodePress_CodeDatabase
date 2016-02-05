6cb10f9f37aeb9f6b7040330bfa16bab6ed99e3f
Luiz <argentinaluiz@gmail.com>
2016-02-05 17:18:57 -0200

    Criando CriteriaCollectionInterface

---
+++ src/CodeDatabase/Contracts/CriteriaCollection.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodePress\CodeDatabase\Contracts\CriteriaInterface;
+
+interface CriteriaCollection
+{
+    public function addCriteria(CriteriaInterface $criteria);
+
+    public function getCriteriaCollection();
+
+    public function getByCriteria(CriteriaInterface $criteriaInterface);
+
+    public function applyCriteria();
+}
