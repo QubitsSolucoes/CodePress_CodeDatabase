f5bfd761df381bf2d6455bc0f3d6af4c74d76e94
Luiz <argentinaluiz@gmail.com>
2016-02-05 16:11:16 -0200

    Criando CriteriaInterface

--- tests/CodeDatabase/CriteriaInterfaceTest.php
+++ tests/CodeDatabase/CriteriaInterfaceTest.php
@@ -4,11 +4,12 @@ namespace CodePress\CodeDatabase\Tests;
 
 use CodePress\CodeCategory\Repository\CategoryRepository;
 use CodePress\CodeDatabase\Contracts\RepositoryInterface;
+use CodePress\CodeDatabase\CriteriaInterface;
 use CodePress\CodeDatabase\Models\Category;
 use Illuminate\Database\Query\Builder;
 use Mockery as m;
 
-class AbstractCriteriaTest extends AbstractTestCase
+class CriteriaInterfaceTest extends AbstractTestCase
 {
 
     public function test_should_apply()
@@ -16,7 +17,7 @@ class AbstractCriteriaTest extends AbstractTestCase
         $mockQueryBuilder = m::mock(Builder::class);
         $mockRepository = m::mock(CategoryRepository::class);
         $mockModel = m::mock(Category::class).
-        $mock = m::mock(AbstractCriteria::class);
+        $mock = m::mock(CriteriaInterface::class);
         $mock->shouldReceive('apply')
             ->with($mockModel,$mockRepository)
             ->andReturn($mockQueryBuilder);
