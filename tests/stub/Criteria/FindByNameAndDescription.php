4788874d3e4ce36d0d56d452fe1aea96679c0304
Luiz <argentinaluiz@gmail.com>
2016-02-05 17:06:27 -0200

    Testando Stub Criteria

--- tests/stub/Criteria/FindByNameAndDescription.php
+++ tests/stub/Criteria/FindByNameAndDescription.php
@@ -5,10 +5,21 @@ namespace CodePress\CodeDatabase\Criteria;
 use CodePress\CodeDatabase\Contracts\CriteriaInterface;
 use CodePress\CodeDatabase\Contracts\RepositoryInterface;
 
-class FindByNameAndDescription implements CriteriaInterface{
+class FindByNameAndDescription implements CriteriaInterface
+{
+
+    private $name;
+    private $description;
+
+    public function __construct($name, $description)
+    {
+        $this->name = $name;
+        $this->description = $description;
+    }
 
     public function apply($model, RepositoryInterface $repository)
     {
-        // TODO: Implement apply() method.
+        return $model->where('name', $this->name)
+            ->where('description', $this->description);
     }
 }
